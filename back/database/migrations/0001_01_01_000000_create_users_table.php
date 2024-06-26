b5302423038f7aa2bc2527ad216aece58009b32e
cannellette <cannelle.garcia@gmail.com>
2024-06-26 16:56:17 +0200

    connexion à la bdd

--- back/database/migrations/0001_01_01_000000_create_users_table.php
+++ back/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,38 +12,44 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id')-> primary();
             $table->string('name');
+            $table->string('firstname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('adress');
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            
+           
+           
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id')-> primary();
+            $table->string('name');
+            $table->string('dimension');
+            $table->string('material');
+            $table->string('color');
+            $table->text('description');
+            $table->decimal('price',total:8,places:2);
+            $table->binary('picture');
         });
 
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+        Schema::create('command', function (Blueprint $table) {
+            $table->increments('id_command')->primary();
+            $table->date('command_date');
+            //clé étrangére
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('product_id')->reference('id')->on('product');
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
-    }
+    // public function down(): void
+    // {
+    //     Schema::dropIfExists('users');
+    //     Schema::dropIfExists('product');
+    //     Schema::dropIfExists('command');
+    // }
 };
