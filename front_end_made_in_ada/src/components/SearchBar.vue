97e3a398615209dfd09db2871e495f0d5d0074cf
AlbaMate <alba.dutel@gmail.com>
2024-06-28 15:20:22 +0200

    add-search

---
+++ front_end_made_in_ada/src/components/SearchBar.vue
@@ -0,0 +1,38 @@
+
+<template>
+    <div class="search-bar">
+      <input
+        type="search"
+        v-model="query"
+        @input="onSearch"
+        placeholder="Rechercher..."
+      />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SearchBar',
+    data() {
+      return {
+        query: ''
+      };
+    },
+    methods: {
+      onSearch() {
+        this.$emit('search', this.query);
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .search-bar {
+    margin-bottom: 20px;
+  }
+  input[type="search"] {
+    width: 100%;
+    padding: 10px;
+    box-sizing: border-box;
+  }
+  </style>
