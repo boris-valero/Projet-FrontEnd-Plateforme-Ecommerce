580315d518f412b0178c592850731ab8036e89f2
boris-valero <157314735+boris-valero@users.noreply.github.com>
2024-06-28 10:25:06 +0000

    update_router.js

--- front_end_made_in_ada/src/router/router.js
+++ front_end_made_in_ada/src/router/router.js
@@ -4,12 +4,18 @@ import {createWebHistory} from "vue-router"
 import HomeFeed from "../views/HomeFeed.vue"
 import MeubleDetails from "../views/MeubleDetails.vue"
 import RegistrationForm from "../views/RegistrationForm.vue"
+import MeublesGlobal from "../views/MeublesGlobal.vue"
 
 const routes = [
     {
         path: '/',
         name: "Home",
         component: HomeFeed
+    },
+    {
+        path: '/meubles',
+        name: "Catalogue de tous les meubles",
+        component: MeublesGlobal
     },
     {
         path: '/meuble/:id',
