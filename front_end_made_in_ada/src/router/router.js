eb8a711048a096f04e5412da9d9b2b7cf6e5e163
Ordi <ordi@MacBookPro-c073f894.localdomain>
2024-06-28 14:55:17 +0000

    updates_diverses

--- front_end_made_in_ada/src/router/router.js
+++ front_end_made_in_ada/src/router/router.js
@@ -4,12 +4,20 @@ import {createWebHistory} from "vue-router"
 import HomeFeed from "../views/HomeFeed.vue"
 import MeubleDetails from "../views/MeubleDetails.vue"
 import RegistrationForm from "../views/RegistrationForm.vue"
+import MeublesGlobal from "../views/MeublesGlobal.vue"
+import MeContacter from "../views/MeContacter.vue"
+
 
 const routes = [
     {
         path: '/',
         name: "Home",
         component: HomeFeed
+    },
+    {
+        path: '/meubles',
+        name: "Catalogue de tous les meubles",
+        component: MeublesGlobal
     },
     {
         path: '/meuble/:id',
@@ -21,6 +29,12 @@ const routes = [
         name: "Registration Form",
         component: RegistrationForm
     },
+    {
+        path: '/mecontacter',
+        name: "Me contacter",
+        component: MeContacter
+    },
+
 ]
 
 const router = createRouter({
