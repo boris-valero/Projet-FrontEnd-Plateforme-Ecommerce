0488553ecefc76b00dea6b3f55916c6512bb0453
Emi28000 <cavalino28@gmail.com>
2024-07-02 15:21:49 +0200

    add_route

---
+++ back/app/Http/Controllers/meublesController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class meublesController extends Controller
+{
+    public fonction routeTest(){
+        return view('meuble.list')
+    }
+}
--- back/routes/web.php
+++ back/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/testv', function () {
+    return ('welcome');
+});
+
+Route ::get('/test', function (){
+    return 'Eric';
 });
